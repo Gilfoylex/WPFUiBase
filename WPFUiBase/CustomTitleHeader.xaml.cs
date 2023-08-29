ef8b9b2b69f8c16729dc018f1af92d96f8a0a4a9
yangjunxin <yangjunxin@huya.com>
2023-08-29 14:40:11 +0800

    更新demo

---
+++ WPFUiBase/CustomTitleHeader.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFUiBase
+{
+    /// <summary>
+    /// CustomTitleHeader.xaml 的交互逻辑
+    /// </summary>
+    public partial class CustomTitleHeader : UserControl
+    {
+        public CustomTitleHeader()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("自定义按钮点击");
+        }
+    }
+}
